dd18a6f658ad1089926aba5d170558c13f7e913b
Nick DeNardis <nick.denardis@gmail.com>
2016-01-05 08:41:21 -0500

    Formatting an updating timing to allow for Project name to be in place

--- data/index.js
+++ data/index.js
@@ -12,50 +12,54 @@ var pattern = /^https:\/\/(.*?).tpondemand.com\/(.*?)$/
 var m = href.match(pattern);
 var subdomain = m[1];
 
-setInterval(function(){
-  var entity = document.getElementsByClassName('entity-id__link');
-  if (entity.length != 0 && entity[0].innerHTML != entity_id) {
-    entity_id = entity[0].innerHTML;
-    attachEntity();
-  }
-}, 1000);
+setInterval(function () {
+    var entity = document.getElementsByClassName('entity-id__link');
+    if (entity.length != 0 && entity[0].innerHTML != entity_id) {
+        entity_id = entity[0].innerHTML;
+        attachEntity();
+    }
+}, 3000);
 
 // Attach to entity
 var attachEntity = function () {
 
-  // Get the title of the story
-  var entity_title = document.getElementsByClassName('view-header__entity-title')[0].innerHTML;
+    // Get the title of the story
+    var entity_title = document.getElementsByClassName('view-header__entity-title')[0].innerHTML;
 
-  // Get the project for the story
-  var entity_project = '';
-  var project_delay = setInterval(function(){
-    entity_project = document.getElementsByClassName('tau-property-project')[0].innerText;
-  }, 2000);
-  clearInterval(project_delay);
+    // Get the project for the story
+    var entity_project = '';
+    if (document.getElementsByClassName('tau-property-project').length > 0) {
+        entity_project = document.getElementsByClassName('tau-property-project')[0].innerText;
+    }
 
-  div.setAttribute('data-account', JSON.stringify({'id':subdomain}));
-  div.setAttribute('data-project', JSON.stringify({'id':entity_project,'name':entity_project}));
-  div.setAttribute('data-item', JSON.stringify({'id':entity_id.replace(/\D/g,''),'name':entity_id + ' ' + entity_title}));
+    div.setAttribute('data-account', JSON.stringify({'id': subdomain}));
+    div.setAttribute('data-project', JSON.stringify({'id': entity_project, 'name': entity_project}));
+    div.setAttribute('data-item', JSON.stringify({
+        'id': entity_id.replace(/\D/g, ''),
+        'name': entity_id + ' ' + entity_title
+    }));
 
-  // Add the button to the page
-  var el = document.getElementsByClassName('view-header__link')[0];
-  el.parentNode.insertBefore(div, el);
+    // Add the button to the page
+    var el = document.getElementsByClassName('view-header__link')[0];
+    el.parentNode.insertBefore(div, el);
 
-  // Tell Harvest a new timer was added
-  if (document.querySelector("#harvest-messaging")) {
-    var event = new CustomEvent("harvest-event:timers:add", {
-      detail: { element: document.getElementsByClassName('harvest-timer')[0] }
-    });
+    // Tell Harvest a new timer was added
+    if (document.querySelector("#harvest-messaging")) {
+        var event = new CustomEvent("harvest-event:timers:add", {
+            detail: {element: document.getElementsByClassName('harvest-timer')[0]}
+        });
 
-    document.querySelector("#harvest-messaging").dispatchEvent(event);
-  } else {
+        document.querySelector("#harvest-messaging").dispatchEvent(event);
+    } else {
 
-    // Add the tracking script to the page
-    var init = "window._harvestPlatformConfig = { 'applicationName': 'TargetProcess', 'permalink': 'https://%ACCOUNT_ID%.tpondemand.com/entity/%ITEM_ID%'}; var s = document.createElement('script');s.src = '//platform.harvestapp.com/assets/platform.js';s.async = true;var ph = document.getElementsByTagName('script')[0];ph.parentNode.insertBefore(s, ph);";
-    var s = document.createElement('script');s.type = 'text/javascript';s.innerHTML = init;
-    var ph = document.getElementsByTagName('script')[0];
-    ph.parentNode.insertBefore(s, ph);
-  }
+        // Add the tracking script to the page
+        var init = "window._harvestPlatformConfig = { 'applicationName': 'TargetProcess', 'permalink': 'https://%ACCOUNT_ID%.tpondemand.com/entity/%ITEM_ID%'}; var s = document.createElement('script');s.src = '//platform.harvestapp.com/assets/platform.js';s.async = true;var ph = document.getElementsByTagName('script')[0];ph.parentNode.insertBefore(s, ph);";
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.innerHTML = init;
+        var ph = document.getElementsByTagName('script')[0];
+        ph.parentNode.insertBefore(s, ph);
+    }
 }
 
 // Overwrite the Harvest pop up to increase z-index
@@ -63,9 +67,9 @@ var css = '#harvest-iframe-container { z-index: 999999 !important; }',
     head = document.head || document.getElementsByTagName('head')[0],
     style = document.createElement('style');
 style.type = 'text/css';
-if (style.styleSheet){
-  style.styleSheet.cssText = css;
+if (style.styleSheet) {
+    style.styleSheet.cssText = css;
 } else {
-  style.appendChild(document.createTextNode(css));
+    style.appendChild(document.createTextNode(css));
 }
 head.appendChild(style);
