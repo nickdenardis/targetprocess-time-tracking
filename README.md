ed56b44d692e2faa6e2d598e8ece38238704cbda
Nick DeNardis <nick.denardis@gmail.com>
2016-01-05 06:58:59 -0500

    Initial working version

---
+++ README.md
@@ -0,0 +1,29 @@
+TargetProcess 3 Time Tracking
+====================
+
+Add time tracking to TargetProcess 3 cards with [Harvest](http://try.hrv.st/3-12573).
+
+![TargetProcess Time Tracking]()
+
+Usage
+-----
+1. Register the [free Harvest account](http://try.hrv.st/3-12573).
+2. Install the [Google Chrome Extension]().
+3. Whenever you visit the issues page on GitHub a new button appears next to the "Follow" link
+4. With one click you can start to track the time. The dialog also shows the sum of time already spent by you on this ticket.
+5. Harvest provides great [project reports](http://try.hrv.st/3-12573) - with links back to the TargetProcess tickets.
+6. You can track a contract progress based on the estimated time/costs limits, which is very helpful. The system can also send people reminder to fill the time sheets online.
+7. It is also possible to directly create [nice looking invoices](http://try.hrv.st/3-12573) and estimates or track late payments.
+
+Development
+-----------
+
+Code is available in GitHub. Once you download / clone the code to your computer you can alter the functionality and create your own variants of this extension.
+
+Firefox Add-on:
+
+    $ cfx xpi
+
+Chrome Extension:
+
+    $ zip -r targetprocess-time-tracking.zip manifest.json data img lib
---
+++ data/index.js
@@ -0,0 +1,71 @@
+var entity_id = null;
+
+// Button to click
+var div = document.createElement('div');
+div.className = 'harvest-timer tau-container view-header__link tau-followlink';
+div.style.width = 'auto';
+div.style.height = 'auto';
+
+// Get the subdomain of the account
+var href = window.location.href;
+var pattern = /^https:\/\/(.*?).tpondemand.com\/(.*?)$/
+var m = href.match(pattern);
+var subdomain = m[1];
+
+setInterval(function(){
+  var entity = document.getElementsByClassName('entity-id__link');
+  if (entity.length != 0 && entity[0].innerHTML != entity_id) {
+    entity_id = entity[0].innerHTML;
+    attachEntity();
+  }
+}, 1000);
+
+// Attach to entity
+var attachEntity = function () {
+
+  // Get the title of the story
+  var entity_title = document.getElementsByClassName('view-header__entity-title')[0].innerHTML;
+
+  // Get the project for the story
+  var entity_project = '';
+  var project_delay = setInterval(function(){
+    entity_project = document.getElementsByClassName('tau-property-project')[0].innerText;
+  }, 2000);
+  clearInterval(project_delay);
+
+  div.setAttribute('data-account', JSON.stringify({'id':subdomain}));
+  div.setAttribute('data-project', JSON.stringify({'id':entity_project,'name':entity_project}));
+  div.setAttribute('data-item', JSON.stringify({'id':entity_id.replace(/\D/g,''),'name':entity_id + ' ' + entity_title}));
+
+  // Add the button to the page
+  var el = document.getElementsByClassName('view-header__link')[0];
+  el.parentNode.insertBefore(div, el);
+
+  // Tell Harvest a new timer was added
+  if (document.querySelector("#harvest-messaging")) {
+    var event = new CustomEvent("harvest-event:timers:add", {
+      detail: { element: document.getElementsByClassName('harvest-timer')[0] }
+    });
+
+    document.querySelector("#harvest-messaging").dispatchEvent(event);
+  } else {
+
+    // Add the tracking script to the page
+    var init = "window._harvestPlatformConfig = { 'applicationName': 'TargetProcess', 'permalink': 'https://%ACCOUNT_ID%.tpondemand.com/entity/%ITEM_ID%'}; var s = document.createElement('script');s.src = '//platform.harvestapp.com/assets/platform.js';s.async = true;var ph = document.getElementsByTagName('script')[0];ph.parentNode.insertBefore(s, ph);";
+    var s = document.createElement('script');s.type = 'text/javascript';s.innerHTML = init;
+    var ph = document.getElementsByTagName('script')[0];
+    ph.parentNode.insertBefore(s, ph);
+  }
+}
+
+// Overwrite the Harvest pop up to increase z-index
+var css = '#harvest-iframe-container { z-index: 999999 !important; }',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+style.type = 'text/css';
+if (style.styleSheet){
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
